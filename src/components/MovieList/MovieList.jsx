a2bc129ea82e6df613ddbe055d11cd1fbe08a1a9
Konstantin Rybakov <konstantin.rybakov@nmi.com>
2024-12-14 23:23:17 +0200

    fixed back navigation

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function MovieList({ movies }) {
+  const location = useLocation();
   return (
     <ul>
       {movies.map(({ id, title }) => (
         <li key={id}>
-          <Link to={`/movies/${id}`} state="/">
+          <Link to={`/movies/${id}`} state={location}>
             {title}
           </Link>
         </li>
